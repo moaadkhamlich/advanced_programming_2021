ef05d33eeee67f243a25d98db850d23b8ccdccdf
Moaad Khamlich <moaad.khamlich@sissa.it>
2022-02-17 13:56:02 +0100

    adding documentation and copy/move semantics for _iterator class

--- exam/stack_pool.hpp
+++ exam/stack_pool.hpp
@@ -1,7 +1,5 @@
-#include <algorithm>
-#include <iostream>
+// relevant headers
 #include <iterator>
-#include <utility>
 #include <vector>
 
 /// \file
@@ -10,52 +8,151 @@
 /// Directory containing the header and source files for the stack_pool<T,N>
 /// class.
 
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+/*---------------------------------------------------------------------------*\
+                        Class template _iterator<P,T>
+\*---------------------------------------------------------------------------*/
+
+/** @brief The class _iterator is used as an iterator for accessing the elements
+    elements of a stack within stack_pool.
+
+    The class _iterator is used as an iterator for accessing the elements
+    of a stack within stack_pool. This is done using as template
+    parameters, the stack_pool type and the value type of value stored in each
+    node of the stacks, which however does not match the template parameter of
+    stack_pool<T,N>, in fact the difference may reside in the const
+    attribute, that allows to obtain const_iterator */
+
 template <typename P, typename T>
 class _iterator {
+  // private members
+ private:
+  /// stack_type deduced from the template parameter P=stack_pool<T,N>
   using stack_type = typename P::stack_type;
+
+  /// the "pointer" used to access element inside the stack_pool data-structure
   stack_type current;
+
+  /// a raw pointer to stack_pool
   P* pool;
 
  public:
-  using value_type = T;
-  using reference = value_type&;
-  using pointer = value_type*;
-  using difference_type = std::ptrdiff_t;
-  using iterator_category = std::forward_iterator_tag;
+  /// Destructor
+  /// destructor is always noexcept, we do nothing because there
+  /// is no resource allocation
   ~_iterator() noexcept {}
 
-  _iterator(P* p, stack_type nd) : current(nd), pool(p){};
+  /// typename used to be compliant with standard library
+  using value_type = T;
+  /// typename used to be compliant with standard library
+  using reference = value_type&;
+  /// typename used to be compliant with standard library
+  using pointer = value_type*;
+  /// typename used to be compliant with standard library
+  using difference_type = std::ptrdiff_t;
+  /// typename used to be compliant with standard library
+  using iterator_category = std::forward_iterator_tag;
 
-  _iterator& operator++() {  // pre-increment
+  /// Defined constructor for the _iterator class.
+  /// we check to have a valid iterator by dereferencing it.
+  /// Because of this check we cannot mark the constructor as noexcept since
+  /// stack_pool.value may throw.
+  _iterator(P* p, stack_type nd) : current(nd), pool(p) {
+    if (!p->empty(nd))
+      *(*this);
+  };
+
+  /// Default constructor
+  _iterator() : current(stack_type(0)), pool(nullptr){};
+
+  /// Default copy constructor is fine, member-wise copy.
+  /// This is marked as noexcept because we assume that the iterator we are
+  /// copying has already been successfully created
+  _iterator(const _iterator&) noexcept = default;
+
+  /// Move constructor
+  /// We cannot use the default one because we have to set pool to nullptr
+  /// This is marked as noexcept because we are just moving a supposed valid
+  /// and already created iterator
+  _iterator(_iterator&& that) noexcept
+      : current(std::move(that.current)), pool(that.pool) {
+    that.current = stack_type(0);
+    that.pool = nullptr;
+  };
+
+  /// Move assignment
+  /// This is marked as noexcept because we are just moving a supposed valid
+  /// and already created iterator
+  _iterator& operator=(_iterator&& that) noexcept {
+    current = std::move(that.current);
+    pool = that.pool;
+    that.current = stack_type(0);
+    that.pool = nullptr;
+    return *this;
+  }
+
+  /// Copy assignment
+  /// This is marked as noexcept because we are using copy constructor and
+  /// move assignment, both noexcept
+  _iterator& operator=(const _iterator& that) {
+    // copy constructor
+    _iterator tmp = that;
+    // move assignment
+    (*this) = std::move(tmp);
+    return *this;
+  }
+
+  /// operator pre-increment: throws an exception if iterator=
+  /// stack_pool.end()
+  _iterator& operator++() {
     current = pool->next(current);
     return *this;
   }
 
-  _iterator operator++(int) {  // post-increment
+  /// operator post-increment: throws an exception if iterator=
+  /// stack_pool.end()
+  _iterator operator++(int) {
     auto tmp = *this;
     ++(*this);
     return tmp;
   }
 
-  reference operator*() { return pool->value(current); }
-  typename P::node_t* operator->() { return &(pool->node(current)); }
+  /// Dereference operator which return the value of the pointed node.
+  /// This cannot be marked noexcept because stack_pool.value may throw
+  reference operator*() const { return pool->value(current); }
 
-  friend bool operator==(const _iterator& a, const _iterator& b) {
-    return (a.current == b.current);
+  /// operator -> used to access the member of the pointed node, e.x. "next"
+  /// this cannot be marked as noexcept because stack_pool.node may throw
+  typename P::node_t* operator->() const { return &(pool->node(current)); }
+
+  /// The equality operator for two iterators.
+  /// Two iterators are equal if they refer to the same pool and point to
+  /// the same node.
+  friend bool operator==(const _iterator& a, const _iterator& b) noexcept {
+    return ((a.current == b.current) && (a.pool == b.pool));
   }
 
-  friend bool operator!=(const _iterator& a, const _iterator& b) {
+  /// The inequality operator for two iterators.
+  /// Two iterators are not equal if they don't refer to the same pool or
+  /// they do not point to the same node.
+  friend bool operator!=(const _iterator& a, const _iterator& b) noexcept {
     return !(a == b);
   }
 };
 
-// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+// //
 
 /*---------------------------------------------------------------------------*\
                         Class template stack_pool<T,N>
 \*---------------------------------------------------------------------------*/
 
-///The pool stores each node in a std::vector<node_t>. The "address" of a node is 1+idx, where idx is the index where the node is stored in the vector. This trick allows us to use address 0 as end, so we can use unsigned integers type. The first node stored in the vector will be put at idx == 0, but it will be referenced as 1.
+/// The pool stores each node in a std::vector<node_t>. The "address" of a
+/// node is 1+idx, where idx is the index where the node is stored in the
+/// vector. This trick allows us to use address 0 as end, so we can use
+/// unsigned integers type. The first node stored in the vector will be put at
+/// idx == 0, but it will be referenced as 1.
 
 /** prova generazione documentazione */
 
@@ -74,14 +171,32 @@ class stack_pool {
     node_t(T&& x, N nxt) : value{std::move(x)}, next{nxt} {};
     explicit node_t(N nxt) : next{nxt} {};
   };
-  using stack_type = N;
-  using value_type = T;
-  std::vector<node_t> pool;
-  using size_type = typename std::vector<node_t>::size_type;
-  stack_type free_nodes;  // at the beginning, it is empty
 
-  node_t& node(stack_type x) noexcept { return pool[x - 1]; }
-  const node_t& node(stack_type x) const noexcept { return pool[x - 1]; }
+  /// the type of the index: it must be an unsigned int, since we are using 0
+  /// as null index
+  using stack_type = N;
+
+  /// the type of the values stored in the stack_pool data structure
+  using value_type = T;
+
+  /// the vector used to store the nodes of each stack
+  std::vector<node_t> pool;
+
+  /// size_type as deduced from the underlying templated vector
+  using size_type = typename std::vector<node_t>::size_type;
+
+  /// the stack of free_nodes
+  stack_type free_nodes;
+
+  /// Function that returns a reference to the node corresponding to a given
+  /// index. I have removed the noexcept keyword since vector::operator[] may
+  /// throw
+  node_t& node(stack_type x) { return pool[x - 1]; }
+
+  /// function that returns a const reference to the node corresponding to a
+  /// given index. I have removed the noexcept keyword since
+  /// vector::operator[] may throw
+  const node_t& node(stack_type x) const { return pool[x - 1]; }
 
   template <typename P_, typename T_>
   friend class _iterator;
@@ -170,6 +285,7 @@ class stack_pool {
       return x;
 
     iterator bottom = begin(x);
+    // empy for loop used to reach the bottom of the stack
     for (; bottom->next != end(); ++bottom)
       ;
     bottom->next = free_nodes;
